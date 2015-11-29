1ee50679b2c306516de5474586ad4903e9b7e563
riyvitxgt <zhukangmin@hotmail.com>
2015-11-29 23:23:54 +0800

    add indexPage

--- dist/js/table-edit.js
+++ dist/js/table-edit.js
@@ -6,6 +6,10 @@ $(function(){
 
 function removeColumn(){
   var deleteTrs = $("#table-content table tbody .selectBox:checked");
+  $("#table-content #index-detail").removeClass("active");
+  $("#table-content #table-detail").addClass("active");
+  $("#table-detail-triggle").addClass("active");
+  $("#index-detail-triggle").removeClass("active");
   if(deleteTrs.size() == 0){
     alert("请选择要删除的行");
   }else{
@@ -16,6 +20,10 @@ function removeColumn(){
 }
 
 function addColumn(){
+  $("#table-content #index-detail").removeClass("active");
+  $("#table-content #table-detail").addClass("active");
+  $("#table-detail-triggle").addClass("active");
+  $("#index-detail-triggle").removeClass("active");
   var trHtml = '<tr>' +
     '<td><input class="selectBox" type="checkbox"></td>' +
     '<td><input type="text"/></td>' +
@@ -35,5 +43,42 @@ function addColumn(){
     '<td><input type="text"/></td>' +
     '</tr>';
 
-  $("#table-content table tbody").append(trHtml);
+  $("#table-detail table tbody").append(trHtml);
+}
+
+function removeIndex(){
+  $("#table-content #table-detail").removeClass("active");
+  $("#table-content #index-detail").addClass("active");
+  $("#index-detail-triggle").addClass("active");
+  $("#table-detail-triggle").removeClass("active");
+  var deleteTrs = $("#index-detail table tbody .selectBox:checked");
+  if(deleteTrs.size() == 0){
+    alert("请选择要删除的行");
+  }else{
+    deleteTrs.each(function(){
+      $(this).parent().parent().remove();
+    })
+  }
+}
+
+function addIndex(){
+  $("#table-content #table-detail").removeClass("active");
+  $("#table-content #index-detail").addClass("active");
+  $("#index-detail-triggle").addClass("active");
+  $("#table-detail-triggle").removeClass("active");
+  var trHtml ='<tr>' +
+  '<td><input class="selectBox" type="checkbox"/></td>' +
+    '<td><input type="text"/></td>' +
+    '<td><input type="text"/></td>' +
+    '<td>' +
+    '<select>' +
+    '<option>Unique</option>' +
+    '<option>Normal</option>' +
+    '<option>Full Text</option>' +
+  '</select>' +
+  '</td>' +
+  '<td><input type="checkbox"/></td>' +
+    '</tr>';
+
+  $("#index-detail table tbody").append(trHtml);
 }
